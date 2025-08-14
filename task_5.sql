4e28f996b42ec53a7320d7327b9e365a491e8766
Mahider Tsegaye <Mahidernardos@gmail.com>
2025-08-14 11:51:46 +0300

    populating the database edited

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,5 @@
 USE alx_book_store;
--- inserts single row in the table 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- insert single row 
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
